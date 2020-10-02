45cf5e3df432ddf70fe89fd17675dc5cc0b3a7f0
zahidyew <zahidyew96@gmail.com>
2020-10-02 22:33:45 +0800

    add elixir cost to cards' attributes & players related endpoints

--- CR-cards.js
+++ CR-cards.js
@@ -1,125 +1,432 @@
 const cards = {
-   "Knight": "Common",
-   "Archers": "Common",
-   "Goblins" : "Common",
-   "Giant": "Rare",
-   "P.E.K.K.A": "Epic",
-   "Minions": "Common",
-   "Balloon": "Epic",
-   "Witch": "Epic",
-   "Barbarians": "Common",
-   "Golem": "Epic",
-   "Skeletons": "Common",
-   "Valkyrie": "Rare",
-   "Skeleton Army": "Epic",
-   "Bomber": "Common",
-   "Musketeer": "Rare",
-   "Baby Dragon": "Epic",
-   "Prince": "Epic",
-   "Wizard": "Rare",
-   "Mini P.E.K.K.A": "Rare",
-   "Spear Goblins": "Common",
-   "Giant Skeleton": "Epic",
-   "Hog Rider": "Rare",
-   "Minion Horde": "Common",
-   "Ice Wizard": "Legendary",
-   "Royal Giant": "Common",
-   "Guards": "Epic",
-   "Princess": "Legendary",
-   "Dark Prince": "Epic",
-   "Three Musketeers": "Rare",
-   "Lava Hound": "Legendary",
-   "Ice Spirit": "Common",
-   "Fire Spirits": "Common",
-   "Miner": "Legendary",
-   "Sparky": "Legendary",
-   "Bowler": "Epic",
-   "Lumberjack": "Legendary",
-   "Battle Ram": "Rare",
-   "Inferno Dragon": "Legendary",
-   "Ice Golem": "Rare",
-   "Mega Minion": "Rare",
-   "Dart Goblin": "Rare",
-   "Goblin Gang": "Common",
-   "Electro Wizard": "Legendary",
-   "Elite Barbarians": "Common",
-   "Hunter": "Epic",
-   "Executioner": "Epic",
-   "Bandit": "Legendary",
-   "Royal Recruits": "Common",
-   "Night Witch": "Legendary",
-   "Bats": "Common",
-   "Royal Ghost": "Legendary",
-   "Ram Rider": "Legendary",
-   "Zappies": "Rare",
-   "Rascals": "Common",
-   "Cannon Cart": "Epic",
-   "Mega Knight": "Legendary",
-   "Skeleton Barrel": "Common",
-   "Flying Machine": "Rare",
-   "Wall Breakers": "Epic",
-   "Royal Hogs": "Rare",
-   "Goblin Giant": "Epic",
-   "Fisherman": "Legendary",
-   "Magic Archer": "Legendary",
-   "Electro Dragon": "Epic",
-   "Firecracker": "Common",
-   "Elixir Golem": "Rare",
-   "Battle Healer": "Rare",
-   "Skeleton Dragons": "Common",
-   "Cannon": "Common",
-   "Goblin Hut": "Rare",
-   "Mortar": "Common",
-   "Inferno Tower": "Rare",
-   "Bomb Tower": "Rare",
-   "Barbarian Hut": "Rare",
-   "Tesla": "Common",
-   "Elixir Collector": "Rare",
-   "X-Bow": "Epic",
-   "Tombstone": "Rare",
-   "Furnace": "Rare",
-   "Goblin Cage": "Rare",
-   "Fireball": "Rare",
-   "Arrows": "Common",
-   "Rage": "Epic",
-   "Rocket": "Rare",
-   "Goblin Barrel": "Epic",
-   "Freeze": "Epic",
-   "Mirror": "Epic",
-   "Lightning": "Epic",
-   "Zap": "Common",
-   "Poison": "Epic",
-   "Graveyard": "Legendary",
-   "The Log": "Legendary",
-   "Tornado": "Epic",
-   "Clone": "Epic",
-   "Earthquake": "Rare",
-   "Barbarian Barrel": "Epic",
-   "Heal Spirit": "Rare",
-   "Giant Snowball": "Common",
-   "Royal Delivery": "Common",
+   "Knight": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Archers": { 
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Goblins": {
+      "rarity": "Common",
+      "elixir": 2,
+   },
+   "Giant": {
+      "rarity": "Rare",
+      "elixir": 5,
+   },
+   "P.E.K.K.A": {
+      "rarity": "Epic",
+      "elixir": 7,
+   },
+   "Minions": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Balloon": {
+      "rarity": "Epic",
+      "elixir": 5,
+   },
+   "Witch": {
+      "rarity": "Epic",
+      "elixir": 5,
+   },
+   "Barbarians": {
+      "rarity": "Common",
+      "elixir": 5,
+   },
+   "Golem": {
+      "rarity": "Epic",
+      "elixir": 8,
+   },
+   "Skeletons": {
+      "rarity": "Common",
+      "elixir": 1,
+   },
+   "Valkyrie": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Skeleton Army": {
+      "rarity": "Epic",
+      "elixir": 3,
+   },
+   "Bomber": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Musketeer": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Baby Dragon": {
+      "rarity": "Epic",
+      "elixir": 4,
+   },
+   "Prince": {
+      "rarity": "Epic",
+      "elixir": 5,
+   },
+   "Wizard": {
+      "rarity": "Rare",
+      "elixir": 5,
+   },
+   "Mini P.E.K.K.A": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Spear Goblins": {
+      "rarity": "Common",
+      "elixir": 2,
+   },
+   "Giant Skeleton": {
+      "rarity": "Epic",
+      "elixir": 6,
+   },
+   "Hog Rider": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Minion Horde": {
+      "rarity": "Common",
+      "elixir": 5,
+   },
+   "Ice Wizard": {
+      "rarity": "Legendary",
+      "elixir": 3,
+   },
+   "Royal Giant": {
+      "rarity": "Common",
+      "elixir": 6,
+   },
+   "Guards": {
+      "rarity": "Epic",
+      "elixir": 3,
+   },
+   "Princess": {
+      "rarity": "Legendary",
+      "elixir": 3,
+   },
+   "Dark Prince": {
+      "rarity": "Epic",
+      "elixir": 4,
+   },
+   "Three Musketeers": {
+      "rarity": "Rare",
+      "elixir": 9,
+   },
+   "Lava Hound": {
+      "rarity": "Legendary",
+      "elixir": 7,
+   },
+   "Ice Spirit": {
+      "rarity": "Common",
+      "elixir": 1,
+   },
+   "Fire Spirits": {
+      "rarity": "Common",
+      "elixir": 2,
+   },
+   "Miner": {
+      "rarity": "Legendary",
+      "elixir": 3,
+   },
+   "Sparky": {
+      "rarity": "Legendary",
+      "elixir": 6,
+   },
+   "Bowler": {
+      "rarity": "Epic",
+      "elixir": 5,
+   },
+   "Lumberjack": {
+      "rarity": "Legendary",
+      "elixir": 4,
+   },
+   "Battle Ram": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Inferno Dragon": {
+      "rarity": "Legendary",
+      "elixir": 4,
+   },
+   "Ice Golem": {
+      "rarity": "Rare",
+      "elixir": 2,
+   },
+   "Mega Minion": {
+      "rarity": "Rare",
+      "elixir": 3,
+   },
+   "Dart Goblin": {
+      "rarity": "Rare",
+      "elixir": 3,
+   },
+   "Goblin Gang": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Electro Wizard": {
+      "rarity": "Legendary",
+      "elixir": 4,
+   },
+   "Elite Barbarians": {
+      "rarity": "Common",
+      "elixir": 6,
+   },
+   "Hunter": {
+      "rarity": "Epic",
+      "elixir": 4,
+   },
+   "Executioner": {
+      "rarity": "Epic",
+      "elixir": 5,
+   },
+   "Bandit": {
+      "rarity": "Legendary",
+      "elixir": 3,
+   },
+   "Royal Recruits": {
+      "rarity": "Common",
+      "elixir": 7,
+   },
+   "Night Witch": {
+      "rarity": "Legendary",
+      "elixir": 4,
+   },
+   "Bats": {
+      "rarity": "Common",
+      "elixir": 2,
+   },
+   "Royal Ghost": {
+      "rarity": "Legendary",
+      "elixir": 3,
+   },
+   "Ram Rider": {
+      "rarity": "Legendary",
+      "elixir": 5,
+   },
+   "Zappies": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Rascals": {
+      "rarity": "Common",
+      "elixir": 5,
+   },
+   "Cannon Cart": {
+      "rarity": "Epic",
+      "elixir": 5,
+   },
+   "Mega Knight": {
+      "rarity": "Legendary",
+      "elixir": 7,
+   },
+   "Skeleton Barrel": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Flying Machine": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Wall Breakers": {
+      "rarity": "Epic",
+      "elixir": 2,
+   },
+   "Royal Hogs": {
+      "rarity": "Rare",
+      "elixir": 5,
+   },
+   "Goblin Giant": {
+      "rarity": "Epic",
+      "elixir": 6,
+   },
+   "Fisherman": {
+      "rarity": "Legendary",
+      "elixir": 3,
+   },
+   "Magic Archer": {
+      "rarity": "Legendary",
+      "elixir": 4,
+   },
+   "Electro Dragon": {
+      "rarity": "Epic",
+      "elixir": 5,
+   },
+   "Firecracker": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Elixir Golem": {
+      "rarity": "Rare",
+      "elixir": 3,
+   },
+   "Battle Healer": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Skeleton Dragons": {
+      "rarity": "Common",
+      "elixir": 4,
+   },
+   "Cannon": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Goblin Hut": {
+      "rarity": "Rare",
+      "elixir": 5,
+   },
+   "Mortar": {
+      "rarity": "Common",
+      "elixir": 4,
+   },
+   "Inferno Tower": {
+      "rarity": "Rare",
+      "elixir": 5,
+   },
+   "Bomb Tower": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Barbarian Hut": {
+      "rarity": "Rare",
+      "elixir": 7,
+   },
+   "Tesla": {
+      "rarity": "Common",
+      "elixir": 4,
+   },
+   "Elixir Collector": {
+      "rarity": "Rare",
+      "elixir": 6,
+   },
+   "X-Bow": {
+      "rarity": "Epic",
+      "elixir": 6,
+   },
+   "Tombstone": {
+      "rarity": "Rare",
+      "elixir": 3,
+   },
+   "Furnace": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Goblin Cage": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Fireball": {
+      "rarity": "Rare",
+      "elixir": 4,
+   },
+   "Arrows": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
+   "Rage": {
+      "rarity": "Epic",
+      "elixir": 2,
+   },
+   "Rocket": {
+      "rarity": "Rare",
+      "elixir": 6,
+   },
+   "Goblin Barrel": {
+      "rarity": "Epic",
+      "elixir": 3,
+   },
+   "Freeze": {
+      "rarity": "Epic",
+      "elixir": 4,
+   },
+   "Mirror": {
+      "rarity": "Epic",
+      "elixir": "Cost of previous card played +1",
+   },
+   "Lightning": {
+      "rarity": "Epic",
+      "elixir": 6,
+   },
+   "Zap": {
+      "rarity": "Common",
+      "elixir": 2,
+   },
+   "Poison": {
+      "rarity": "Epic",
+      "elixir": 4,
+   },
+   "Graveyard": {
+      "rarity": "Legendary",
+      "elixir": 5,
+   },
+   "The Log": {
+      "rarity": "Legendary",
+      "elixir": 2,
+   },
+   "Tornado": {
+      "rarity": "Epic",
+      "elixir": 3,
+   },
+   "Clone": {
+      "rarity": "Epic",
+      "elixir": 3,
+   },
+   "Earthquake": {
+      "rarity": "Rare",
+      "elixir": 3,
+   },
+   "Barbarian Barrel": {
+      "rarity": "Epic",
+      "elixir": 2,
+   },
+   "Heal Spirit": {
+      "rarity": "Rare",
+      "elixir": 1,
+   },
+   "Giant Snowball": {
+      "rarity": "Common",
+      "elixir": 2,
+   },
+   "Royal Delivery": {
+      "rarity": "Common",
+      "elixir": 3,
+   },
 }
 
 function assignRarity(item) {
    if (cards.hasOwnProperty(item.name)) {
       //console.log(item.name)
-      Object.assign(item, { rarity: cards[item.name] })
+      Object.assign(item, { rarity: cards[item.name].rarity })
+      Object.assign(item, { elixirCost: cards[item.name].elixir })
    } else {
       return item;
    }
 }
 
-function setRarity(data, res) {
+function queryFilter(items, query) {
+   let myArray = []
+
+   for(let i = 0; i < items.length; i++) {
+      if (items[i].rarity.toLowerCase() == query.rarity || items[i].rarity == query.rarity) {
+         myArray.push(items[i])
+      }
+   }
+   return myArray
+}
+
+function setRarity(data, res, req) {
    // add new key/value pair to an object
    data.map(assignRarity)
-   /* let name = []
 
-   for(let i = 0; i < data.length; i ++) {
-      name[i] = data[i].name
-   }
-   res.json(name) */
-
-   res.json(data)
+   // check if the request has query attached to it
+   if (Object.keys(req.query).length === 0) { res.json(data) }
+   else { res.json(queryFilter(data, req.query)) }
+   
+   //console.log(req.query)
    //console.log(json.items[0].name)
 }
 
--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function checkError(response) {
 }
 
 //https://api.clashroyale.com/v1/clans?minMembers=40
+// get all the cards in CR
 app.get('/cards', (req, res) => {
    let url = 'https://proxy.royaleapi.dev/v1/cards'
    
@@ -28,14 +29,62 @@ app.get('/cards', (req, res) => {
       }
    })
       .then(response => checkError(response))
-      .then(json => CR_cards.setRarity(json.items, res))
+      .then(json => CR_cards.setRarity(json.items, res, req))
+      .catch((error) => console.log(error))
+})
+
+// .../players?id= . Need player's id without the #. Get player's info
+app.get('/players', (req, res) => {
+   let url = 'https://proxy.royaleapi.dev/v1/players/%23' + req.query.id
+
+   fetch(url, {
+      method: 'GET',
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': ' Bearer ' + process.env.TOKEN
+      }
+   })
+      .then(response => checkError(response))
+      .then(json => res.json(json))
+      .catch((error) => console.log(error))
+})
+
+// get player's battlelog
+app.get('/players/battlelog', (req, res) => {
+   let url =  `https://proxy.royaleapi.dev/v1/players/%23${req.query.id}/battlelog`
+
+   fetch(url, {
+      method: 'GET',
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': ' Bearer ' + process.env.TOKEN
+      }
+   })
+      .then(response => checkError(response))
+      .then(json => res.json(json))
+      .catch((error) => console.log(error))
+})
+
+// get player's upcoming chests
+app.get('/players/chests', (req, res) => {
+   let url = `https://proxy.royaleapi.dev/v1/players/%23${req.query.id}/upcomingchests`
+
+   fetch(url, {
+      method: 'GET',
+      headers: {
+         'Content-Type': 'application/json',
+         'Authorization': ' Bearer ' + process.env.TOKEN
+      }
+   })
+      .then(response => checkError(response))
+      .then(json => res.json(json))
       .catch((error) => console.log(error))
 })
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-   console.log("Server startrd on port " + PORT)
+   console.log("Server started on port " + PORT)
 })
 
 
