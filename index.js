18405ad1356a0e125939684e3bce7bf7b97703e8
zahidyew <zahidyew96@gmail.com>
2020-10-03 04:13:01 +0800

    refactor fetch function

--- index.js
+++ index.js
@@ -16,69 +16,46 @@ function checkError(response) {
    }
 }
 
-//https://api.clashroyale.com/v1/clans?minMembers=40
-// get all the cards in CR
-app.get('/cards', (req, res) => {
-   let url = 'https://proxy.royaleapi.dev/v1/cards'
-   
+function goFetch(url, req, res, mode) {
    fetch(url, {
       method: 'GET',
       headers: {
          'Content-Type': 'application/json',
-         'Authorization': ' Bearer ' + process.env.TOKEN  
+         'Authorization': ' Bearer ' + process.env.TOKEN
       }
    })
       .then(response => checkError(response))
-      .then(json => CR_cards.setRarity(json.items, res, req))
+      .then(json => {
+         if(mode === "default") { res.json(json) }  
+         else if (mode === "setRarityFirst") { CR_cards.setRarity(json.items, res, req) }
+         else { console.log("Passed mode is incorrect") };
+      })
       .catch((error) => console.log(error))
+}
+
+//https://api.clashroyale.com/v1/clans?minMembers=40
+// get all the cards in CR
+app.get('/cards', (req, res) => {
+   let url = 'https://proxy.royaleapi.dev/v1/cards'
+   goFetch(url, req, res, "setRarityFirst")
 })
 
 // .../players?id= . Need player's id without the #. Get player's info
 app.get('/players', (req, res) => {
    let url = 'https://proxy.royaleapi.dev/v1/players/%23' + req.query.id
-
-   fetch(url, {
-      method: 'GET',
-      headers: {
-         'Content-Type': 'application/json',
-         'Authorization': ' Bearer ' + process.env.TOKEN
-      }
-   })
-      .then(response => checkError(response))
-      .then(json => res.json(json))
-      .catch((error) => console.log(error))
+   goFetch(url, req, res, "default")
 })
 
 // get player's battlelog
 app.get('/players/battlelog', (req, res) => {
    let url =  `https://proxy.royaleapi.dev/v1/players/%23${req.query.id}/battlelog`
-
-   fetch(url, {
-      method: 'GET',
-      headers: {
-         'Content-Type': 'application/json',
-         'Authorization': ' Bearer ' + process.env.TOKEN
-      }
-   })
-      .then(response => checkError(response))
-      .then(json => res.json(json))
-      .catch((error) => console.log(error))
+   goFetch(url, req, res, "default")
 })
 
 // get player's upcoming chests
 app.get('/players/chests', (req, res) => {
    let url = `https://proxy.royaleapi.dev/v1/players/%23${req.query.id}/upcomingchests`
-
-   fetch(url, {
-      method: 'GET',
-      headers: {
-         'Content-Type': 'application/json',
-         'Authorization': ' Bearer ' + process.env.TOKEN
-      }
-   })
-      .then(response => checkError(response))
-      .then(json => res.json(json))
-      .catch((error) => console.log(error))
+   goFetch(url, req, res, "default")
 })
 
 const PORT = process.env.PORT || 5000;
@@ -86,5 +63,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
    console.log("Server started on port " + PORT)
 })
-
-
